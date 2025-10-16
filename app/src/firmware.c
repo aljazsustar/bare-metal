6ca126e28d7fa0b2dbc5fcd679001012b948d801
Aljaz <aljazsustar99@gmail.com>
2025-10-16 13:51:23 +0200

    Split project into more files

--- app/src/firmware.c
+++ app/src/firmware.c
@@ -3,23 +3,10 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/flash.h>
 #include <libopencm3/stm32/gpio.h>
-#include <libopencm3/cm3/systick.h>
-#include <libopencm3/cm3/vector.h>
+#include "core/system.h"
 
 #define LED_PORT 0x48000000
 #define LED_PIN  1 << 5
-#define CPU_FREQ 96000000
-#define SYSTICK_FREQ 1000
-
-volatile uint64_t ticks = 0;
-
-void sys_tick_handler(void) {
-    ticks++;
-}
-
-static uint64_t get_ticks(void) {
-    return ticks;
-}
 
 static void gpio_setup(void)
 {
@@ -28,25 +15,17 @@ static void gpio_setup(void)
     gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_PIN);
 }
 
-static void systick_setup(void) {
-    systick_set_frequency(SYSTICK_FREQ, CPU_FREQ);
-    systick_counter_enable();
-    systick_interrupt_enable();
-}
-
 int main(void)
 {
-    rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_3V3_96MHZ]);
-
+    system_setup();
     gpio_setup();
-    systick_setup();
 
-    uint64_t start_time = get_ticks();
+    uint64_t start_time = system_get_ticks();
 
     while (1) {
-        if (get_ticks() - start_time >= 1000) {
+        if (system_get_ticks() - start_time >= 1000) {
             gpio_toggle(LED_PORT, LED_PIN);
-            start_time = get_ticks();
+            start_time = system_get_ticks();
         }
     }
 
